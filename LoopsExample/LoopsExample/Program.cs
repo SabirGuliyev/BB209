5437be537bba41e7e757a0e08b95b89ecff3a64c
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-03-11 17:36:35 +0400

    Loops added

---
+++ LoopsExample/LoopsExample/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+#region For
+//int count =5;
+//int sum = 0;
+//for (int i = 1; i < 100; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        //count++;
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+//Console.WriteLine(count); 
+#endregion
+
+#region While Do-While
+//int num = 0;
+//int count = 0;
+
+//while (num != 0)
+//{
+//    num /= 10;
+//    count++;
+//}
+
+//Console.WriteLine(count);
+
+//do
+//{
+//    num /= 10;
+//    count++;
+//} while (num != 0);
+
+//Console.WriteLine(count);
+
+//Console.WriteLine("1.Balans");
+//Console.WriteLine("2.Nagdlashdirmaq");
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("0.Exit");
+
+//string choice = Console.ReadLine();
+
+//while (choice != "0")
+//{
+//    Console.Clear();
+//    Console.WriteLine("1.Balans");
+
+//    Console.WriteLine("2.Nagdlashdirmaq");
+//    Console.WriteLine();
+//    Console.WriteLine();
+//    Console.WriteLine("0.Exit");
+//     choice = Console.ReadLine();
+//}
+
+
+//string choice = "";
+//do
+//{
+//    Console.Clear();
+
+//    Console.WriteLine("1.Balans");
+//    Console.WriteLine("2.Nagdlashdirmaq");
+//    Console.WriteLine();
+//    Console.WriteLine();
+//    Console.WriteLine("0.Exit");
+
+//    choice = Console.ReadLine();
+//}
+//while (choice != "0"); 
+#endregion
+
