74a8877f3bd04430b503560b92361e422e13c3f0
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-04-11 20:25:01 +0400

    Task added

---
+++ HomeTask/HomeTask/Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+
+namespace HomeTask
+{
+    internal class Program
+    {
+        public static string Path = @"C:\Users\sabir\Desktop\BB209\HomeTask\HomeTask\Files\names.json";
+        static void Main(string[] args)
+        {
+            //Add("Ibrahim");
+            //Delete("Ali");
+
+        }
+
+        public static void Add(string name)
+        {
+            List<string> names = Deserialize(Path);
+            names.Add(name);
+            Serialize(names, Path);
+
+        }
+        public static void Delete(string name)
+        {
+
+            List<string> names = Deserialize(Path);
+
+            names.Remove(name);
+
+            Serialize(names, Path);
+
+        }
+
+
+        public static bool Search(Predicate<string> predicate)
+        {
+            List<string> items=Deserialize(Path);
+            return items.Exists(predicate);
+        }
+
+
+        public static List<string> Deserialize(string path)
+        {
+            string result;
+            using (StreamReader sr = new(path))
+            {
+                result = sr.ReadToEnd();
+            }
+            List<string> names = JsonConvert.DeserializeObject<List<string>>(result);
+            return names;
+        }
+        public static void Serialize(List<string> names,string path)
+        {
+            string json = JsonConvert.SerializeObject(names);
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(json);
+            }
+        }
+       
+    }
+}
---
+++ MultithreadingExample/MultithreadingExample/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace MultithreadingExample
+{
+    internal class Program
+    {
+        
+        public static int Count = 0;
+        public static object LockObject = new();
+        public static object LockObject2 = new();
+        static async Task Main(string[] args)
+        {
+            //    Thread thread = new Thread(GetFirst);
+            //    Thread thread2 = new Thread(GetSecond);
+
+            //    thread.Start();
+            //    thread2.Start();
+
+
+
+            //    thread.Join();
+            //    thread2.Join();
+
+            //    Console.WriteLine(Count);
+
+
+
+
+            Task task=RunTask();
+            Task task2=RunTask2();
+
+           await Task.WhenAll(task, task2);
+
+           
+
+
+
+
+            Console.WriteLine("Program finito");
+
+
+
+
+
+
+
+
+
+        }
+
+        public static async Task RunTask()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    Console.WriteLine($"1-ci dovr donur {i}");
+                }
+            });
+        }
+        public static async Task RunTask2()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    Console.WriteLine($"2-ci {i}");
+                }
+            });
+        }
+        public static void GetFirst()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (LockObject2)
+                {
+                    lock (LockObject)
+                    {
+                        Count++;
+                    }
+                }
+             
+              
+                //Thread.Sleep(1000);
+                //Console.WriteLine($"1-ci dovr donur {i}");
+            }
+            //Monitor.Enter(LockObject);
+            //try
+            //{
+            //    Count++;
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+            //finally
+            //{
+            //    Monitor.Exit(LockObject);
+            //}
+
+
+        }
+        public static void GetSecond()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (LockObject)
+                {
+                    lock (LockObject2)
+                    {
+                        Count--;
+                    }
+                }
+                //Console.WriteLine($"2-ci {i}");
+            }
+        }
+    }
+}
